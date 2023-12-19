24ae328d610c6cf82666df93c58d8000b08f7e39
Lolya <100037587+Cyanola@users.noreply.github.com>
2023-12-19 03:27:32 +0300

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.observer {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-
-
-    opens com.example.observer to javafx.fxml;
-    exports com.example.observer;
-}
--- src/main/java/observerComponents/Infastructure/Observer.java
+++
@@ -1,7 +0,0 @@
-package observerComponents.Infastructure;
-
-import java.util.Date;
-
-public interface Observer {
-    public abstract void update(Date date);
-}
